af6617555c81e4c92dcfbde953c94ffea99d2e9f
Zee V. (Philip) <zee@vall.in>
2020-12-19 22:28:49 +0100

    fix: use the right keys for the difficulties

--- Modules/RaidData.lua
+++ Modules/RaidData.lua
@@ -1,9 +1,9 @@
 -- Raid difficulties define what different kind of raid difficulties exists.
 RAID_DIFFICULTIES = {
-    ["R"] = "Raid Finder",
-    ["N"] = "Normal",
-    ["H"] = "Heroic",
-    ["M"] = "Mythic"
+    ["RAIDFINDER"] = "Raid Finder",
+    ["NORMAL"] = "Normal",
+    ["HEROIC"] = "Heroic",
+    ["MYTHIC"] = "Mythic"
 }
 
 -- Raid instances is a map of raid instances, their encounters and their difficulties.
@@ -15,52 +15,52 @@ RAID_INSTANCES = {
             {
                 id = "2418",
                 name = "Huntsman Altimor",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2417",
                 name = "Stone Legion Generals",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2412",
                 name = "The Council of Blood",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2407",
                 name = "Sire Denathrius",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2406",
                 name = "Lady Inerva Darkvein",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2405",
                 name = "Artificer Xy'mox",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2402",
                 name = "Sun King's Salvation",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2399",
                 name = "Sludgefist",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2398",
                 name = "Shriekwing",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             },
             {
                 id = "2383",
                 name = "Hungering Destroyer",
-                difficulties = {"R", "N", "H", "M"}
+                difficulties = {"RAIDFINDER", "NORMAL", "HEROIC", "MYTHIC"}
             }
         }
     }
